d6a526f110a3a9edad609612c4c0e78d4083e05c
Alex_jr <alex.webdev.13@gmail.com>
2023-03-09 21:00:53 +0200

    init laravel 8.75

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use App\Providers\RouteServiceProvider;
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
@@ -11,26 +13,20 @@ class RedirectIfAuthenticated
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  string|null  ...$guards
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle(Request $request, Closure $next, ...$guards)
     {
-      switch ($guard) {
-        case 'admin':
-          if(Auth::guard($guard)->check()){
-           return redirect()->route('dashboard.index');
-          }
-        break;
+        $guards = empty($guards) ? [null] : $guards;
 
-        default:
-          if (Auth::guard($guard)->check()) {
-            return redirect('/');
-          }
-        break;
-      }
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                return redirect(RouteServiceProvider::HOME);
+            }
+        }
 
-      return $next($request);
+        return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProjectsController;
+
+use App\Http\Controllers\Auth\AdminLoginController;
+
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\OrderController as DashboardOrderController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,33 +21,33 @@
 |
 */
 
-Route::get('/', 'IndexController@index')->name('home');
+Route::get('/', [IndexController::class, 'index'])->name('home');
 
-Route::get('/team', 'IndexController@team')->name('team');
-Route::get('/contacts', 'IndexController@contacts')->name('contacts');
-Route::get('/about', 'IndexController@about')->name('about');
-Route::get('/gallery', 'ProjectsController@gallery')->name('gallery');
+Route::get('/team', [IndexController::class, 'team'])->name('team');
+Route::get('/contacts', [IndexController::class, 'contacts'])->name('contacts');
+Route::get('/about', [IndexController::class, 'about'])->name('about');
+Route::get('/gallery', [ProjectsController::class, 'gallery'])->name('gallery');
 
-Route::get('/projects', 'ProjectsController@index')->name('projects');
-Route::get('/projects/{name}', 'ProjectsController@profile');
+Route::get('/projects', [ProjectsController::class, 'index'])->name('projects');
+Route::get('/projects/{name}', [ProjectsController::class, 'profile']);
 
-Route::get('/projects/{name}/villas', 'ProjectsController@villas');
+Route::get('/projects/{name}/villas', [ProjectsController::class, 'villas']);
 
-Route::get('/contact-form', 'OrderController@contactForm')->name('contact-form');
+Route::get('/contact-form', [OrderController::class, 'contactForm'])->name('contact-form');
 
-Route::post('/contact-form/save', 'OrderController@saveContactForm')->name('contact-form.save');
+Route::post('/contact-form/save', [OrderController::class, 'saveContactForm'])->name('contact-form.save');
 
-Route::get('/contact-form-success', 'OrderController@contactFormSuccess')->name('contact-form-success');
+Route::get('/contact-form-success', [OrderController::class, 'contactFormSuccess'])->name('contact-form-success');
 
 // Admin routes
 Route::group(['prefix' => 'dashboard', 'middleware' => ['guest:admin']], function () {
-	Route::get('/login',  'Auth\AdminLoginController@showLoginForm')->name('login');
-	Route::post('/login', 'Auth\AdminLoginController@login'        )->name('dashboard.login.submit');
+	Route::get('/login',  [AdminLoginController::class, 'showLoginForm'])->name('login');
+	Route::post('/login', [AdminLoginController::class, 'login'])->name('dashboard.login.submit');
 });
 Route::group(['prefix' => 'dashboard', 'middleware' => ['auth:admin']], function () {
-	Route::get('/', 'Dashboard\DashboardController@index')->name('dashboard');
-  Route::get('/orders', 'Dashboard\OrderController@index')->name('dashboard.orders');
+	Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+  Route::get('/orders', [DashboardOrderController::class, 'index'])->name('dashboard.orders');
 
-  Route::post('/logout', 'Auth\AdminLoginController@logout')->name('dashboard.logout');
-	Route::get('/logout', 'Auth\AdminLoginController@logout')->name('dashboard.logout');
+  Route::post('/logout', [AdminLoginController::class, 'logout'])->name('dashboard.logout');
+	Route::get('/logout', [AdminLoginController::class, 'logout'])->name('dashboard.logout');
 });
